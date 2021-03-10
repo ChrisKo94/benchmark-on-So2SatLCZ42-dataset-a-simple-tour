19511ed8b00062ca582f6fbd9a984e14c654dad2
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-03-10 12:19:52 +0100

    removed loop

--- train.py
+++ train.py
@@ -43,85 +43,81 @@ batchSize=64
 #mode = "all"
 mode = "urban"
 uncertain = False
-entropy_quantile = np.array([0.3, 0.4, 0.5, 0.6, 0.7]) # choose quantile of most certain images (w.r.t. voter entropy) for training, requires mode = "urban"
+entropy_quantile = 0.4# choose quantile of most certain images (w.r.t. voter entropy) for training, requires mode = "urban"
 
-for i in range(len(entropy_quantile)):
+train_data = h5py.File(train_file, 'r')
+x_train = np.array(train_data.get("x"))
+y_train = np.array(train_data.get("y"))
 
-    train_data = h5py.File(train_file, 'r')
-    x_train = np.array(train_data.get("x"))
-    y_train = np.array(train_data.get("y"))
+validation_data = h5py.File(validation_file, 'r')
+x_val = np.array(validation_data.get("x"))
+y_val = np.array(validation_data.get("y"))
 
-    validation_data = h5py.File(validation_file, 'r')
-    x_val = np.array(validation_data.get("x"))
-    y_val = np.array(validation_data.get("y"))
+if mode == "urban":
+    indices_train = np.where(np.where(y_train == np.amax(y_train, 0))[1] + 1 < 11)[0]
+    x_train = x_train[indices_train, :, :, :]
+    y_train = y_train[indices_train]
+    indices_val = np.where(np.where(y_val == np.amax(y_val, 0))[1] + 1 < 11)[0]
+    x_val = x_val[indices_val, :, :, :]
+    y_val = y_val[indices_val]
 
-    if mode == "urban":
-        indices_train = np.where(np.where(y_train == np.amax(y_train, 0))[1] + 1 < 11)[0]
-        x_train = x_train[indices_train, :, :, :]
-        y_train = y_train[indices_train]
-        indices_val = np.where(np.where(y_val == np.amax(y_val, 0))[1] + 1 < 11)[0]
-        x_val = x_val[indices_val, :, :, :]
-        y_val = y_val[indices_val]
+if entropy_quantile > 0 and mode == "urban":
+    entropies = h5py.File(path_data + "entropies_train.h5", 'r')
+    entropies_train = np.array(entropies.get("entropies_train"))
+    entropies_train = entropies_train[indices_train]
+    entropies_train[np.where(np.isnan(entropies_train))] = 0
 
-    if entropy_quantile[i] > 0 and mode == "urban":
-        entropies = h5py.File(path_data + "entropies_train.h5", 'r')
-        entropies_train = np.array(entropies.get("entropies_train"))
-        entropies_train = entropies_train[indices_train]
-        entropies_train[np.where(np.isnan(entropies_train))] = 0
-
-        entropies = pd.DataFrame({"entropies": entropies_train,
-                                  "order": np.arange(len(y_train))})
-        if uncertain == False:
-            entropies = entropies.sort_values(by=['entropies'])
-        else:
-            entropies = entropies.sort_values(by=['entropies'], ascending=False)
-        ## Order training data accordingly
-        idx = np.array(entropies["order"])
-        ## Cut off at given quantile
-        idx = idx[:np.floor(entropy_quantile[i] * len(idx)).astype(int)]
-        x_train = x_train[idx, :, :, :]
-        y_train = y_train[idx]
-
-    'number of all samples in training and validation sets'
-    trainNumber=y_train.shape[0]
-    validationNumber=y_val.shape[0]
-
-    lrate = 0.002
-    lr_sched = lr.step_decay_schedule(initial_lr=lrate, decay_factor=0.5, step_size=5)
-
-    ###################################################
-    # patch_shape=(32,32,10)
-    # model = resnet.resnet_v2(input_shape=patch_shape, depth=11, num_classes=numClasses)
-    if mode == "urban":
-        model = model.sen2LCZ_drop(depth=17, dropRate=0.2, fusion=1, num_classes=10)
+    entropies = pd.DataFrame({"entropies": entropies_train,
+                              "order": np.arange(len(y_train))})
+    if uncertain == False:
+        entropies = entropies.sort_values(by=['entropies'])
     else:
-        model = model.sen2LCZ_drop(depth=17, dropRate=0.2, fusion=1)
+        entropies = entropies.sort_values(by=['entropies'], ascending=False)
+    ## Order training data accordingly
+    idx = np.array(entropies["order"])
+    ## Cut off at given quantile
+    idx = idx[:np.floor(entropy_quantile * len(idx)).astype(int)]
+    x_train = x_train[idx, :, :, :]
+    y_train = y_train[idx]
+
+'number of all samples in training and validation sets'
+trainNumber=y_train.shape[0]
+validationNumber=y_val.shape[0]
+
+lrate = 0.002
+lr_sched = lr.step_decay_schedule(initial_lr=lrate, decay_factor=0.5, step_size=5)
+
+###################################################
+# patch_shape=(32,32,10)
+# model = resnet.resnet_v2(input_shape=patch_shape, depth=11, num_classes=numClasses)
+if mode == "urban":
+    model = model.sen2LCZ_drop(depth=17, dropRate=0.2, fusion=1, num_classes=10)
+else:
+    model = model.sen2LCZ_drop(depth=17, dropRate=0.2, fusion=1)
 
 
-    model.compile(optimizer = Nadam(), loss = 'categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer = Nadam(), loss = 'categorical_crossentropy', metrics=['accuracy'])
 
-    early_stopping = EarlyStopping(monitor = 'val_loss', patience = 40)
+early_stopping = EarlyStopping(monitor = 'val_loss', patience = 40)
 
-    PATH = file0 + "Sen2LCZ_" + str(batchSize) + "_lr_" + str(lrate)
-    if mode == "urban":
-        PATH = PATH + "_urban"
+PATH = file0 + "Sen2LCZ_" + str(batchSize) + "_lr_" + str(lrate)
+if mode == "urban":
+    PATH = PATH + "_urban"
 
-    if entropy_quantile[i] > 0:
-        if uncertain == True:
-            PATH = PATH + "_most_uncertain_" + str(entropy_quantile[i])
-        else:
-            PATH = PATH + "_most_certain_" + str(entropy_quantile[i])
-    modelbest = PATH + "_weights_best.hdf5"
+if entropy_quantile > 0:
+    if uncertain == True:
+        PATH = PATH + "_most_uncertain_" + str(entropy_quantile)
+    else:
+        PATH = PATH + "_most_certain_" + str(entropy_quantile)
+modelbest = PATH + "_weights_best.hdf5"
 
-    checkpoint = ModelCheckpoint(modelbest, monitor='val_accuracy', verbose=1, save_best_only=True,
-                                 save_weights_only=True, mode='auto', save_freq='epoch')
+checkpoint = ModelCheckpoint(modelbest, monitor='val_accuracy', verbose=1, save_best_only=True,
+                             save_weights_only=True, mode='auto', save_freq='epoch')
 
-    print(i)
-
-    model.fit(generator(x_train, y_train, batchSize=batchSize, num=trainNumber, mode=mode),
-                    steps_per_epoch = trainNumber//batchSize,
-                    validation_data= generator(x_val, y_val, num=validationNumber, batchSize=batchSize, mode=mode),
-                    validation_steps = validationNumber//batchSize,
-                    epochs=100,
-                    max_queue_size=100,
-                    callbacks=[early_stopping, checkpoint, lr_sched])
+model.fit(generator(x_train, y_train, batchSize=batchSize, num=trainNumber, mode=mode),
+                steps_per_epoch = trainNumber//batchSize,
+                validation_data= generator(x_val, y_val, num=validationNumber, batchSize=batchSize, mode=mode),
+                validation_steps = validationNumber//batchSize,
+                epochs=100,
+                max_queue_size=100,
+                callbacks=[early_stopping, checkpoint, lr_sched])
