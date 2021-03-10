d4d53e02f38b12d39acd96fd214b593a2757aed3
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-03-10 12:51:09 +0100

    change entropy quantile

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ batchSize=64
 #mode = "all"
 mode = "urban"
 uncertain = False
-entropy_quantile = 0.4# choose quantile of most certain images (w.r.t. voter entropy) for training, requires mode = "urban"
+entropy_quantile = 0.5# choose quantile of most certain images (w.r.t. voter entropy) for training, requires mode = "urban"
 
 train_data = h5py.File(train_file, 'r')
 x_train = np.array(train_data.get("x"))
