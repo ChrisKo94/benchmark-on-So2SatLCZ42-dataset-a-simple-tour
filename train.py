54119973fd921338bbfe7baf8fa8b2675b45cada
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-03-29 16:04:05 +0200

    set entropy to 0

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ batchSize=64
 #mode = "all"
 mode = "urban"
 uncertain = False
-entropy_quantile = 0.7# choose quantile of most certain images (w.r.t. voter entropy) for training, requires mode = "urban"
+entropy_quantile = 0# choose quantile of most certain images (w.r.t. voter entropy) for training, requires mode = "urban"
 
 train_data = h5py.File(train_file, 'r')
 x_train = np.array(train_data.get("x"))
